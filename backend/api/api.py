0870ab500d5b6471b81f5d851726e611e9783506
ppfuark <phfuark2007@gmail.com>
2025-05-26 16:54:14 -0300

    Backend | Deleting files

--- backend/api/api.py
+++
@@ -1,7 +0,0 @@
-from api.routes import upload, query
-from fastapi import APIRouter
-
-api_router = APIRouter()
-
-api_router.include_router(upload.router, prefix="/api")
-api_router.include_router(query.router, prefix="/api")
